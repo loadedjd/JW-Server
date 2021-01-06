d16fba4f6be0b5f494339f9a794d9cddba101569
Jared Williams <williamsjared62@gmail.com>
2021-01-06 03:09:57 -0500

    Updated docker file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,16 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getIndex(@Res() res: Response): string {
+
+    res.sendFile('index.html', { root: 'web/build' })
+
+    return "";
+  }
 }
