889ae4983172701114eb4cbe98af1480b51fcd05
Jared Williams <williamsjared62@gmail.com>
2021-01-05 23:35:55 -0500

    Trying to fix the app engine networking issues

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 8080;
 
-  await app.listen(80);
+  await app.listen(port);
 }
 bootstrap();
