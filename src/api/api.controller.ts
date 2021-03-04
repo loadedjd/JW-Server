d35a6197ab4fb5208ae3010a7774c9e180c61ce8
Jared Williams <williamsjared62@gmail.com>
2021-03-04 00:08:11 -0500

    Test

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Controller('api')
 export class ApiController {
+  @Get()
+  index(@Res() res: Response): string {
+    res.json({
+      system: 'admin',
+      status: 'up',
+    });
 
-    @Get()
-    index(@Res() res: Response): string {
+    return '';
+  }
 
-        res.json({
-            system: 'admin',
-            status: 'up'
-        })
-
-        return "";
-    }
+  @Post()
+  incomingCall(): string {
+    return '<?xml version="1.0" encoding="UTF-8"?><Response><Say>Thanks for calling!</Say></Response>';
+  }
 }
